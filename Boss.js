d2f332387e94de569139daf0bfe722820ac9796d
sniegockajulia <julia.sniegocka@usp.ceu.es>
2024-10-09 22:08:28 +0200

    Update Boss.js

--- Boss.js
+++ Boss.js
@@ -1,14 +1,8 @@
-
 // Boss.js
 class Boss extends Opponent {
     constructor(game) {
-        this.speed *= 2;
-        this.myImage = 'assets/boss.png',
-        this.myImageDead = 'assets/boss_dead.png';
-       
-        // Call the parent class constructor with the boss's properties
-        super(game, width, height, x, y, speed, myImage, myImageDead);
+        super(game);  // Call the Opponent constructor
+        this.speed *= 2;  // Boss moves twice as fast
+        this.image.src = 'assets/boss.png';  // Use the boss image
     }
-
-    // The shoot and collide methods can be inherited directly from the Opponent class.
 }
