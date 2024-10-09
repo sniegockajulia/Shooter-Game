7b1d4d9b26857e2fd82f1ee39f598f32858dffc6
sniegockajulia <julia.sniegocka@usp.ceu.es>
2024-10-09 22:32:36 +0200

    Update Boss.js

--- Boss.js
+++ Boss.js
@@ -1,9 +1,13 @@
 // Boss.js
 class Boss extends Opponent {
     constructor(game) {
-        super(game, width, height, x, y, speed, myImage, myImageDead);  // Call the Opponent constructor
+        super(game);  // Call the Opponent constructor
         this.speed *= 2;  // Boss moves twice as fast
-        this.myImage = 'assets/boss.png';
-        this.myImageDead = 'assets/boss_dead.png';
+        this.image.src = 'assets/boss.png';  // Use the boss image
+    }
+
+    collide() {
+        super.collide();  // Call the parent's collide method
+        document.getElementById('game-over-image').src = 'assets/boss_dead.png';
     }
 }
